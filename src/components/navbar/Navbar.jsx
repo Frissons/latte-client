b2cd1d11bbd7f1ba00c1075197d2be298f0347e9
frissons <dodongbren@gmail.com>
2022-02-10 17:25:39 +0800

    added navbar features and updated the css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,16 +9,20 @@ const Navbar = () => {
     <div className="latte__navbar">
       <div className='latte__navbar-links'>
         <div className='latte__navbar-links_logo'>
-          <img src={logo} alt="logo" />
-          <a href="#home" >Latte</a>
+          {/* <img src={logo} alt="logo" /> */}
+          <p><a href="#home" >☕ Latte</a></p>
         </div>
         <div className="latte__navbar-links_container">
-          <p><a  href="#home" >🍫 Home</a></p>
-          <p><a  href="#WLatte" >☕ What is Latte</a></p>
-          <p><a  href="#Possibility" >🍺 Encryption</a></p>
-          <p><a  href="#Features" >🍵 Features</a></p>
-          <p><a  href="#Blog" >📚 Library</a></p>
+          <p><a href="#home" >🍫 Home</a></p>
+          <p><a href="#WLatte" >🍷 What is Latte</a></p>
+          <p><a href="#Possibility" >🍺 Encryption</a></p>
+          <p><a href="#Features" >🍵 Features</a></p>
+          <p><a href="#Blog" >📚 Library</a></p>
         </div>
+      </div>
+      <div className='latte__navbar-sign'>
+        <p><a href="#Blog" >🥙 Sign in</a></p>
+        <button type='button'>Sign up</button>
       </div>
     </div>
   )
